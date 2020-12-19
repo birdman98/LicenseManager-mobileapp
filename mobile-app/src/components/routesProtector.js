c358c8a7d33ef6f3492dff9156e2f37260c58b02
Piotr Matras <piotrmatras21@gmail.com>
2020-12-19 20:10:37 +0100

    created initial routes config

---
+++ mobile-app/src/components/routesProtector.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Spinner from '../components/spinner';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const RoutesProtector = ({ isAuthenticated }) => {
+  return isAuthenticated ? (
+    <Tab.Navigator>
+      <Tab.Screen name="Licenses" component={Spinner} />
+      <Tab.Screen name="Customers" component={Spinner} />
+      <Tab.Screen name="Settings" component={Spinner} />
+    </Tab.Navigator>
+  ) : (
+    <Stack.Navigator>
+      <Stack.Screen name="Log in" component={Spinner} />
+    </Stack.Navigator>
+  );
+};
+
+RoutesProtector.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = ({ authentication }) => ({
+  isAuthenticated: authentication.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(RoutesProtector);
